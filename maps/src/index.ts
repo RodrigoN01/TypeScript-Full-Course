6c1855355d46a104bb835f9311c67199d5387b59
Rodrigo Novaes <rod_novaes@hotmail.com>
2022-01-03 11:49:28 +1300

    Initial setup for the maps app, display the map and created the User and Company classes

---
+++ maps/src/index.ts
@@ -0,0 +1,11 @@
+/// <reference types="@types/google.maps" />
+// import { User } from './User';
+// import { Company } from './Company';
+
+new google.maps.Map(document.getElementById('map'), {
+  zoom: 1,
+  center: {
+    lat: 0,
+    lng: 0,
+  },
+});
